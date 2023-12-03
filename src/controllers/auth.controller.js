c24f4f600f88b65310818816d89653cf360d578e
MiguelR29 <mromero47@alumnos.uaq.mx>
2023-12-03 11:32:36 -0600

    Creación del Login y Logout

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs/dist/bcrypt.js';
 import {createAccessToken} from '../libs/jwt.js';
+import mongoose from "mongoose";
+
 
 export const register = async (req, res) => {
     const {username, email, password, names, lastname, age} = req.body;
@@ -44,7 +46,45 @@ export const register = async (req, res) => {
     
 };
 
-export const login = (req, res) => res.send("login");
+export const login = async (req, res) => {
+    const {email, password} = req.body;
 
-import mongoose from "mongoose";
+    try {
 
+        //Variable para verificar que exista el usuario
+        const userFound = await User.findOne({email: email});
+
+        //Validar el usuario
+        if (!userFound) return res.status(400).json({message: "User not Found"});
+
+        const isMatch = await bcrypt.compare(password, userFound.password);
+
+        if(!isMatch) return res.status(400).json({message: "Incorrect password"});
+
+        //Crear el token con la función importada al inicio y creada en jwt.js
+        const token = await createAccessToken({id: userFound._id});
+
+        //Devolver al front la cookie con el token
+        //Devolver al front el json con los datos a utilizar
+        res.cookie('token', token);
+
+        res.json({
+            id: userFound._id, 
+            username: userFound.username, 
+            email: userFound.email
+        });
+
+    } catch (error) {
+        res.status(500).json({message: error.message});
+
+    }
+    
+};
+
+export const logout = async(req, res) => { 
+    res.cookie('token', "", {
+        expires: new Date(0)
+    }); 
+
+    return res.sendStatus(200);
+}
