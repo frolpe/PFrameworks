36e1c204d1138af0502e9d3bf578d12e16416671
MiguelR29 <miguelromamigazo4@gmail.com>
2023-12-05 14:42:04 -0600

    Función al Login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return(
     <BrowserRouter>
       <Routes>
           <Route path='/' element={<h1>Home Page</h1>} />
-          <Route path='/login' element={<h1>Login</h1>} />
-          <Route path='/register' element={<h1>Register</h1>} />
+          <Route path='/login' element={<LoginPage/>} />
+          <Route path='/register' element={<RegisterPage/>} />
           <Route path='/skills' element={<h1>Skills Page</h1>} />
           <Route path='/add-skill' element={<h1>New Skill</h1>} />
           <Route path='/skills/:id' element={<h1>Update Task</h1>} />
