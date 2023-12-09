ac1a71fa857ee969ef635e94c4c547740ab45bdf
Francisco Olvera <frolpe@outlook.com>
2023-12-08 22:59:15 -0600

    CSS cardProjects

--- client/src/components/projectCardP.jsx
+++ client/src/components/projectCardP.jsx
@@ -6,7 +6,18 @@ function ProjectCard2({ project }) {
         <div>
             <p style={{ 'fontSize': '20px', 'margin': '2px' }}>{project.names}</p>
             <p>{project.description}</p>
-            <Link>{project.link}</Link>
+            <div>
+                <Link to={project.link} style={{ 'text-decoration': 'none', 'color': '#e3e5d7' }}
+                    onMouseEnter={(e) => {
+                        e.target.style.color = '#586875';
+                    }}
+                    onMouseLeave={(e) => {
+                        e.target.style.color = '#e3e5d7';
+                    }
+                    }
+                    target="_blank" 
+                    rel="noopener noreferrer">{project.link}</Link>
+            </div>
             <hr />
         </div>
     )
